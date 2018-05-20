cac945e9c1bebb1716ee7dd84e3787153b8474ba
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2018-05-20 18:25:17 -0400

    Add Triangle function

---
+++ Fuzzy/Function/Triangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzzy.Function
+{
+    public class Triangle : Trapezoid
+    {
+        public Triangle(double? a, double? b, double? c) : base(a, b, c) { }
+    }
+}

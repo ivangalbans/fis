1d733c86d8f7ec95f6548ea7a9849c116018dffd
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2018-05-21 12:24:51 -0400

    Implement models sugeno and tsukamoto

--- Fuzzy/Model/ModelMethod.cs
+++ Fuzzy/Model/ModelMethod.cs
@@ -17,7 +17,7 @@ namespace Fuzzy.Model
         /// <param name="function">Dictionary which associates a linguistic value with its corresponding membership function</param>
         /// <param name="step">Distance between values to discretize function's domains</param>
         /// <returns>Dictionary that associates each output variable name with its calculated numeric value</returns>
-        public static Dictionary<string, double> Mamdani(List<(object w, string outputVar, string linguisticVal)> rulesOutput,
+        public static Dictionary<string, double> Mamdani(List<(double w, string outputVar, string linguisticVal)> rulesOutput,
             Func<FunctionBase, double, double> defuzzy, Dictionary<string, FunctionBase> function, double step=0.1)
         {
             rulesOutput.Sort((a, b) => a.outputVar.CompareTo(b.outputVar));
@@ -35,6 +35,83 @@ namespace Fuzzy.Model
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Sugeno model with weighted average
+        /// </summary>
+        /// <param name="rulesOutput">List of tuples (w, outputVar, z) representing the output of each rule's evaluation</param>
+        /// <returns>Dictionary that associates each output variable name with its calculated numeric value</returns>
+        public static Dictionary<string, double> Sugeno(List<(double w, string outputVar, double z)> rulesOutput)
+        {
+            var num = new Dictionary<string, double>();
+            var den = new Dictionary<string, double>();
+
+            foreach ((var w, var variable, var z) in rulesOutput)
+            {
+                num.TryGetValue(variable, out double lastNum);
+                den.TryGetValue(variable, out double lastDen);
+
+                if (!num.ContainsKey(variable)) num.Add(variable, lastNum);
+                if (!den.ContainsKey(variable)) den.Add(variable, lastDen);
+
+                num[variable] = lastNum + w * z;
+                den[variable] = lastDen + w;
+            }
+            return Divide(num, den);
+        }
+
+        /// <summary>
+        /// Tsukamoto model
+        /// </summary>
+        /// <param name="rulesOutput">List of tuples (w, outputVar, linguisticVal) representing the output of each rule's evaluation</param>
+        /// <param name="function">Dictionary which associates a linguistic value with its corresponding membership function</param>
+        /// <param name="step">Distance between values to discretize function's domains</param>
+        /// <returns>Dictionary that associates each output variable name with its calculated numeric value</returns>
+        public static Dictionary<string, double> Tsukamoto(List<(double w, string outputVar, string linguisticVal)> rulesOutput,
+            Dictionary<string, FunctionBase> function, double step = 0.1)
+        {
+            var num = new Dictionary<string, double>();
+            var den = new Dictionary<string, double>();
+
+            foreach ((var w, var variable, var lingVal) in rulesOutput)
+            {
+                var func = function[lingVal];
+                var points = func.Points(step);
+
+                // Choose the one whose image is closer to 'w'
+                // 'z' is the value of function's domain with image approximately 'w'
+                var z = points.Aggregate((p1, p2) => (Math.Abs(p1.y - w) < Math.Abs(p2.y - w)) ? p1 : p2).x;
+
+                num.TryGetValue(variable, out double lastNum);
+                den.TryGetValue(variable, out double lastDen);
+
+                if (!num.ContainsKey(variable)) num.Add(variable, lastNum);
+                if (!den.ContainsKey(variable)) den.Add(variable, lastDen);
+
+                num[variable] = lastNum + w * z;
+                den[variable] = lastDen + w;
+            }
+
+            return Divide(num, den);
+        }
+
+        /// <summary>
+        /// Divide values corresponding to the same key in two dictionaries
+        /// </summary>
+        /// <param name="num">Dividend dictionary</param>
+        /// <param name="den">Divider dictionary</param>
+        /// <returns>New dictionary with the same keys as the parameters and the divisions results as values</returns>
+        private static Dictionary<string, double> Divide(Dictionary<string, double> num, Dictionary<string, double> den)
+        {
+            var dic = new Dictionary<string, double>();
+            foreach (var variable in num.Keys)
+            {
+                if (!dic.ContainsKey(variable))
+                    dic.Add(variable, num[variable] / den[variable]);
+                dic[variable] = num[variable] / den[variable];
+            }
+            return dic;
         }
     }
 }
